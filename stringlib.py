76856d5320cd2f8c7756a9c5456c7017e50a48a0
blakelucas <lucaswb@appstate.edu>
2022-09-12 18:35:09 -0400

    completed programming assignment

--- stringlib.py
+++ stringlib.py
@@ -3,15 +3,29 @@
 #Add the following functions:
 #
 #reverseStr - takes as input a string and returns the reverse of it
-#
+def reverseStr(word):
+    return word[::-1]
+
 #containsWord - takes as input two strings, containingStr and containedStr,
 #and returns "Yes" if containedStr is anywhere within containingStr 
 #and returns "No" otherwise
-#
+def containsWord(containingStr, containedStr):
+    if containedStr.lower() in containingStr.lower():
+        return "Yes"
+    else:
+        return "No"
+
 #isPalindrome - takes as input a string and returns "Yes" if it is
 #palindrome and "No" otherwise  
-#
+def isPalindrome(word):
+    rev = reverseStr(word).lower()
+    if rev == word:
+        return "Yes"
+    else:
+        return "No"
+
 #upperCaseStr - takes as input a string and returns the identical string
 #with the characters 'a' ... 'z' changed to uppercase
-#
+def upperCaseStr(word):
+    return word.upper()
 
--- worker.py
+++ worker.py
@@ -3,10 +3,13 @@
 from stringlib import *
 
 #Add a Worker class to this file.
-#
+class Worker:
+    def __init__(self, inputStr):
+        self.inputStr = inputStr
+
 #The Worker class constructor needs to take as input
 #a string.  It will set its own data member to that string.
-#
+
 #Add methods to the Worker class that are equivalent to
 #functions in the stringlib module.  These methods will
 #not take a string as input (except for the containsWord
@@ -14,4 +17,15 @@ from stringlib import *
 #these methods will operate on the Worker class data member. 
 #Your method can call the needed function in the stringlib
 #module.
+    def reverseStr(self):
+        return reverseStr(self.inputStr)
+ 
+    def containsWord(self, containedStr):
+        return containsWord(self.inputStr, containedStr)
+
+    def isPalindrome(self):
+        return isPalindrome(self.inputStr)
+
+    def upperCaseStr(self):
+        return upperCaseStr(self.inputStr)
 
